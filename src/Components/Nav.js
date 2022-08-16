2b129f40655b4f8809c5f7000afe85862d984967
Andrew Kal <andrewkalgit@gmail.com>
2022-08-16 01:08:10 -0400

    UI layout complete, need to complete UI and functionalilty for sign up and home changing text and updating years of coaching by year, stats count up over 3-5 sec.

---
+++ src/Components/Nav.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from './ImgFolder/LeoGBoxingLogo.png'
+
+//To Do 
+//Make hamburger icon with animation 
+//Hamburger icon functionality 
+
+function Nav(props) {
+    return (
+        <div>
+            <header class="shadow-sm">
+  <div class="max-w-screen-4xl p-4 mx-auto">
+    <div class="flex items-center justify-between space-x-4 lg:space-x-10">
+      <div class="flex lg:w-0 lg:flex-1">
+        <img class="w-16 h-16 bg-gray-200 rounded-lg" src={Logo} alt='Logo'/>
+      </div>
+
+      <nav class="hidden space-x-8 text-sm font-medium sm:flex">
+        <Link class="text-gray-500 hover:text-black" to="/">Home</Link>
+        <Link class="text-gray-500 hover:text-black" to="/about">About</Link>
+        <Link class="text-gray-500 hover:text-black" to="/fighters">Fighters</Link>
+        <Link class="text-gray-500 hover:text-black" to="/FAQ">FAQ</Link>
+      </nav>
+
+      <div class="items-center justify-end flex-1 hidden space-x-4 sm:flex">
+        <Link 
+          class="px-5 py-2 text-sm font-medium text-blue-400 hover:text-white bg-gray-100 hover:bg-blue-400 rounded-lg"
+          to="signup"
+        >
+          Book Session!
+        </Link>
+      </div>
+
+      <div class="sm:hidden">
+        <button class="p-2 text-gray-600 bg-gray-100 rounded-lg" type="button">
+          <span class="sr-only">Open menu</span>
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewbox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 6h16M4 12h16M4 18h16"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</header>
+        </div>
+    );
+}
+
+export default Nav;
